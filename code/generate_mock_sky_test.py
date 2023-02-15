7cd0181cb2e5d6356659f81c4f92d7baf5cc86a9
BaptisteJost <jost.baptiste@gmail.com>
2023-02-15 18:11:53 +0900

    implementation of beam for mock sky generation

--- code/generate_mock_sky_test.py
+++ code/generate_mock_sky_test.py
@@ -10,21 +10,21 @@ import IPython
 #from config_pipeline_copy import *
 
 nside = 64
-print('NSIDE=',nside)
+print('NSIDE=', nside)
 r = 0.0
 bire_angle = (0.0*u.deg).to(u.rad)
 A_lens = 1
 
-machine = 'idark'
-if machine =='local':
-    path_BB_local = '/home/baptiste/BBPipe'
-elif machine=='NERSC':
-    path_BB_NERSC = '/global/homes/j/jost/BBPipe'
-elif machine=='idark':
-    path_BB_idark = '/home/jost/code/BBPipe'
+machine = 'local'
+if machine == 'local':
+    path_BB = '/home/baptiste/BBPipe'
+elif machine == 'NERSC':
+    path_BB = '/global/homes/j/jost/BBPipe'
+elif machine == 'idark':
+    path_BB = '/home/jost/code/BBPipe'
 else:
     print(machine, ' doesn\'t have BBPipe path specified')
-path_BB = path_BB_idark
+# path_BB = path_BB_idark
 # ps_planck = hp.read_cl(path_BB + '/test_mapbased_param/Cls_Planck2018_lensed_scalar.fits')
 print('test2')
 
@@ -54,37 +54,51 @@ instrument_LB = np.load('data/instrument_LB_IMOv1.npy', allow_pickle=True).item(
 noise_lvl = np.array([instrument_LB[f]['P_sens'] for f in instrument_LB.keys()])
 beam_rad = np.array([instrument_LB[f]['beam']
                      for f in instrument_LB.keys()]) * u.arcmin.to(u.rad)
+# IPython.embed()
 noise_nl = []
+cmb_spectra_beamed = []
+fg_maps_smoothed = []
 for f in range(len(noise_lvl)):
-    Bl = hp.gauss_beam(beam_rad[f], lmax=lmax-1)[2:]
-    Bl = np.ones(Bl.shape)
+    # Bl = hp.gauss_beam(beam_rad[f], lmax=lmax-1)[2:]
+    Bl = hp.gauss_beam(beam_rad[f], lmax=3*nside)
+    # Bl = np.ones(Bl.shape)
     # noise = (noise_lvl[f]*np.pi/60/180)**2 * np.ones(len(ell_noise))
-    noise = (noise_lvl[f]*np.pi/60/180)**2 * np.ones(3*nside)
-    # noise_nl.append(noise / (Bl**2))
-    noise_nl.append(noise)
-noise_nl = np.array(noise_nl)
+    noise = (noise_lvl[f]*np.pi/60/180)**2 * np.ones(3*nside+1)
 
-if machine=='idark':
-    output_dir = '/home/jost/simu/LB_mock/fullsky_nobeam/'
-elif machine=='NERSC':
+    cmb_spectra_beamed.append(cmb_spectra[:, :3*nside+1] / (Bl**2))
+    noise_nl.append(noise / (Bl**2))
+
+    fg_maps_smoothed.append(hp.smoothing(
+        np.array([fg_freq_maps_full[2*f]*0, fg_freq_maps_full[2*f],
+                  fg_freq_maps_full[2*f+1]]), fwhm=beam_rad[f]))
+    # noise_nl.append(noise)
+noise_nl = np.array(noise_nl)
+cmb_spectra_beamed = np.array(cmb_spectra_beamed)
+fg_maps_smoothed = np.array(fg_maps_smoothed)
+
+if machine == 'idark':
+    output_dir = '/home/jost/simu/LB_mock/fullsky_withbeam/'
+elif machine == 'NERSC':
     output_dir = '/global/homes/j/jost/these/pixel_based_analysis/results_and_data/pipeline_data/test_mock/data/'
 else:
-    print(machine,' doesn\'t have a specified output directory ')
+    print(machine, ' doesn\'t have a specified output directory ')
 
 for i in range(99):
-    map_CMB = hp.synfast(cmb_spectra, nside, new=True)[1:]
+    # map_CMB = hp.synfast(cmb_spectra, nside, new=True)[1:]
 
     freq_maps = []
     for f in range(freq_number):
         map_noise = hp.synfast([noise_nl[f]*0, noise_nl[f], noise_nl[f],
                                 noise_nl[f]*0], nside, new=True)[1:]
-        map_f = map_CMB + fg_freq_maps_full[2*f:2*f+1] + map_noise
+        map_CMB = hp.synfast(cmb_spectra_beamed[f], nside, new=True)[1:]
+        # map_f = map_CMB + fg_freq_maps_full[2*f:2*f+1] + map_noise
+        map_f = map_CMB + fg_maps_smoothed[f][1:] + map_noise
         freq_maps.append(map_f[0])
         freq_maps.append(map_f[1])
 
     freq_maps = np.array(freq_maps)
-    #np.save('/global/homes/j/jost/these/pixel_based_analysis/results_and_data/pipeline_data/test_mock/data/mock_LB_nobeam' +
+    # np.save('/global/homes/j/jost/these/pixel_based_analysis/results_and_data/pipeline_data/test_mock/data/mock_LB_nobeam' +
     #        str(i).zfill(4)+'.npy', freq_maps)
-
-    np.save(output_dir + 'mock_LB_nobeam' +
+    # IPython.embed()
+    np.save(output_dir + 'mock_LB' +
             str(i).zfill(4)+'.npy', freq_maps)
