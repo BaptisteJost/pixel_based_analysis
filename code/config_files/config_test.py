a9c1cdfef4a147a8ed06f8c3c1e3dd654f1c6749
BaptisteJost <jost.baptiste@gmail.com>
2023-03-16 10:05:01 +0900

    debugging

--- code/config_files/config_test.py
+++ code/config_files/config_test.py
@@ -204,9 +204,9 @@ jac_cosmo_min = jac_cosmo
 
 '''spectral MCMC options'''
 spectral_MCMC_flag = 1
-nsteps_spectral = 13000
+nsteps_spectral = 30000
 # nsteps_spectral = 2000  # DEBUG LB
-discard_spectral = 5000
+discard_spectral = 10000
 # discard_spectral = 500  # DEBUG LB
 spectral_walker_per_dim = 2
 spectral_dim = freq_number + birefringence_flag + 2*spectral_flag
